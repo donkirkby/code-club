46bedc4e8022e8306bcb2da3254cb06521a05565
Don Kirkby <donkirkby@users.noreply.github.com>
2021-04-16 11:52:03 -0700

    Add stub and tests for first challenge.

---
+++ marriage_and_travelling.py
@@ -0,0 +1,11 @@
+def solve(s):
+    return s
+
+
+def main():
+    print(solve("saalllaaaammmmm"))  # expects "salam"
+    print(solve("z"))  # expects "z"
+    print(solve("aaa"))  # expects "aa"
+
+
+main()
