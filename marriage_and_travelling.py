e05ef0e39559286f24511318c569ddfd62ad1f72
Richard Liang <rliang@bccfe.ca>
2021-04-21 12:36:05 -0700

    Added an iterative method for the solution.

--- marriage_and_travelling.py
+++ marriage_and_travelling.py
@@ -1,14 +1,31 @@
 def recursive_solve(part_string, finished_count):
     if not part_string:
         return ""
-    finished_count+=1
-    output = part_string[0]
+    finished_count += 1
+    first_character = part_string[0]
+    new_part_string = part_string[finished_count:]
 
+    remaining = recursive_solve(new_part_string, finished_count)
+    return first_character + remaining
 
 
 def solve(s):
+    # return recursive_solve(s, 0)
+    return solve_iteratively(s)
 
-    return s
+
+def solve_iteratively(s):
+    remaining_string = s
+    finished_count = 0
+
+    result_string = ""
+    while remaining_string:
+        finished_count += 1
+        first_character = remaining_string[0]
+        remaining_string = remaining_string[finished_count:]
+        result_string += first_character
+
+    return result_string
 
 
 def main():
