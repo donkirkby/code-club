6df5d9cfde4e86b2834f81fb15201d6a408132f1
birgitwatson <bwatson@bccfe.ca>
2021-05-19 13:12:35 -0700

    Add requirements for problem_chooser.py.

--- problem_chooser.py
+++ problem_chooser.py
@@ -1,5 +1,11 @@
+import requests
+import bs4
+
+
 def main():
-    pass
+    response = requests.get("https://www.topcoder.com/tc?module=ProblemArchive&sr=551&er=600")
+    soup = bs4.BeautifulSoup(response.text, 'html.parser')
+    soup.find()
 
 
 main()
